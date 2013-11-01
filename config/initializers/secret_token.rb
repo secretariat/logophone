dd63830b2be0996768e09e0eee7235b431358c7c
«Secretariat1987» <mixey.viti@bigmir.net>
2013-11-01 21:54:58 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logophone::Application.config.secret_token = '3ca272165b2850f42e84547ad2f5f9aaf6a9f9fa3f7b9ec7252e861737753b4a16cba756e7131f5b838528d06a6d1ff5422220d1587b23e309ffd599ce7c67f6'
