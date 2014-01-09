9f625864d26238e99f6c7703c494a929d7dd1343
Andrew Golovin <golovin.010@gmail.com>
2014-01-09 22:39:36 +0200

    finally

--- lib/intro.rb
+++ lib/intro.rb
@@ -119,40 +119,25 @@ class Intro
 			@logo << "/output/#{@number[4]}#{@number[6]}0#{@number[5]}XXXX.png"
 			@logo << "/output/uzor/#{@number[4]}#{@number[6]}0X#{@number[8]}#{@number[7]}XX.png"
 		else
-<<<<<<< HEAD
-			if @number[6] > @number[10] && @number[10] != 0
-				fwrite "+++++++++++++++++++++"
-				@logo << "/output/#{@number[4]}#{@number[10]}#{over_close(@number[10])}#{@number[9]}XXXX.png"
-				@logo << "/output/#{@number[4]}#{@number[6]}0#{@number[5]}XXXX.png"
-				@logo << "/output/uzor/#{@number[4]}#{@number[6]}0X#{@number[8]}#{@number[7]}XX.png"
-=======
 			if @number[6] > @number[10] then
 				if @number[10] == 0 && (@number[6] != 2 || @number[6] != 4 || @number[6] != 6) && ( over_close(@number[6]) == 0 )   then
 					@logo << "/output/#{@number[4]}#{@number[6]}0#{@number[5]}XXXX.png"
-					@logo << "/output/#{color_folder(@number[7])}/#{@number[4]}#{@number[6]}0X#{@number[8]}#{@number[7]}XX.png"
+					@logo << "/output/uzor/#{@number[4]}#{@number[6]}0X#{@number[8]}#{@number[7]}XX.png"
 					@logo << "/output/#{@number[4]}#{@number[10]}#{over_close(@number[10])}#{@number[9]}XXXX.png"
 				else
 					@logo << "/output/#{@number[4]}#{@number[10]}#{over_close(@number[10])}#{@number[9]}XXXX.png"
 					@logo << "/output/#{@number[4]}#{@number[6]}0#{@number[5]}XXXX.png"
-					@logo << "/output/#{color_folder(@number[7])}/#{@number[4]}#{@number[6]}0X#{@number[8]}#{@number[7]}XX.png"
+					@logo << "/output/uzor/#{@number[4]}#{@number[6]}0X#{@number[8]}#{@number[7]}XX.png"
 				end
-<<<<<<< HEAD
->>>>>>> aa1041f32f1e151b746ba74b47fa84d67c0ff50f
-			else
-				@logo << "/output/#{@number[4]}#{@number[6]}#{over_close(@number[6])}#{@number[5]}XXXX.png"
-				@logo << "/output/uzor/#{@number[4]}#{@number[6]}#{over_close(@number[6])}X#{@number[8]}#{@number[7]}XX.png"
-				@logo << "/output/#{@number[4]}#{@number[10]}0#{@number[9]}XXXX.png"
-=======
 			elsif @number[6] == 0 then
 					@logo << "/output/#{@number[4]}#{@number[10]}0#{@number[9]}XXXX.png"
 					@logo << "/output/#{@number[4]}#{@number[6]}#{over_close(@number[6])}#{@number[5]}XXXX.png"
-					@logo << "/output/#{color_folder(@number[7])}/#{@number[4]}#{@number[6]}#{over_close(@number[6])}X#{@number[8]}#{@number[7]}XX.png"
+					@logo << "/output/uzor/#{@number[4]}#{@number[6]}#{over_close(@number[6])}X#{@number[8]}#{@number[7]}XX.png"
 				else
 					@logo << "/output/#{@number[4]}#{@number[6]}#{over_close(@number[6])}#{@number[5]}XXXX.png"
-					@logo << "/output/#{color_folder(@number[7])}/#{@number[4]}#{@number[6]}#{over_close(@number[6])}X#{@number[8]}#{@number[7]}XX.png"
+					@logo << "/output/uzor/#{@number[4]}#{@number[6]}#{over_close(@number[6])}X#{@number[8]}#{@number[7]}XX.png"
 					@logo << "/output/#{@number[4]}#{@number[10]}0#{@number[9]}XXXX.png"
 				# end
->>>>>>> test
 			end
 		end
 	end
